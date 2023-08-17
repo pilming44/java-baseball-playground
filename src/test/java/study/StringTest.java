837f31e234e4bb7acca0ff1e8acd1c9a833d0be5
pilming44 <pilming44@naver.com>
2023-08-17 22:36:56 +0900

    String 클래스에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,65 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1", "2");
+    }
+
+    @Test
+    void splitOnlyOne() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1,4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAtFirst() {
+        char result = "abc".charAt(0);
+        assertThat(result).isEqualTo('a');
+    }
+
+    @Test
+    void charAtMiddle() {
+        char result = "abc".charAt(1);
+        assertThat(result).isEqualTo('b');
+    }
+
+    @Test
+    void charAtLast() {
+        char result = "abc".charAt(2);
+        assertThat(result).isEqualTo('c');
+    }
+
+    @Test
+    void charAtIndexOutOfBounds() {
+        assertThatThrownBy(() -> {
+            char result = "abc".charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
+
+    @Test
+    @DisplayName("이름 표시")
+    void displayName() {
+
     }
 }
